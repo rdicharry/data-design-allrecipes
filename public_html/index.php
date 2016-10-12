4067ada6d5f18806b887a241aa8260f979dece8d
Rebecca Dicharry <rdicharry@gmail.com>
2016-10-12 16:51:38 -0600

    fixed indentation

--- public_html/index.php
+++ public_html/index.php
@@ -17,30 +17,23 @@
 			<!-- begin section 1 -->
 			<section>
 				<h1>Persona</h1>
-				<p> The user is someone who is researching a recipe. They may be looking for something to make for dinner, or planning a special cooking project (a challenging new recipe, something inspired by a recent meal, planning a special meal for a special occasion). They would like to see the recipe neatly laid out and easy to read. they would like the steps to be carefully written for cooking success. They would like to see reviews of the recipe so they can feel more confident that their time spent cooking will be successful.
-
-				</p>
-				<p>Alternatively, another type of user would be a person who is creating a recipe and would like to share it. This user wants it to be easy to enter ingredients and steps. This user would also like to see comments in order to recieve positive reviews and reinforce their creative culinary endeavors. (Or, they may be interested in criticism that they can use constructively in order to improve their cooking outcomes.)
-				</p>
-
-				<p>The persona is someone who likes cooking and enjoys good food. They would like to see photos and recipe suggestions. They would like to be able to search recipes and share them via email/social media.
-				</p>
-
-
-					<ul>
+				<ul>
 					<li><b>Name:</b> James O'Brien</li>
 					<li><b>Age:</b> 42</li>
 					<li><b>Attitudes and Behaviors:</b> James likes good food, enjoys eating, and enjoys cooking. He likes researching the cooking experience, looking for new recipes to try. He enjoys sharing food with and cooking for others. He may spend a substantial amount of time looking for a great recipe to impress his friends and family, or just for himself.</li>
 					<li><b>Technology:</b> James ejoys spending time online, but he does not have the most up-to-date software/computer system.</li>
 					<li><b>Frustrations and Needs:</b> James likes recipes to be easy to read and to search. He wants to be able to read reviews in order to get an idea of how successful a recipe will be (in addition to author's comments describing the outcome).</li>
-					<li><b>Goals:</b>James likes to share recipes he has developed or modified with others. He likes recieving 5-star reviews on creations that are aparticularly successful. He wants to be able to easily share recipes he has created with friends, family, and co-workers.</li>
-					</ul>
-
-				<!--<img src="img/molecule.png" alt="Caffeine and cAMP">-->
+					<li><b>Goals:</b>James likes to share recipes he has developed or modified with others. He likes recieving 5-star reviews on creations that are particularly successful. He wants to be able to easily share recipes he has created with friends, family, and co-workers (via email/social media). James likes to find new recipes and try new ideas and techniques, and working with new tastes.</li>
+				</ul>
 			</section>
 			<section>
 				<h1>Use Case</h1>
-			<p>James is a little bored with his usual go-to recipes. He is cooking a big meal Saturday for his family, and would like to try something new. He has browsed through a list of currently popular recipes, and he has searched for pork shoulder in ingredients because he thinks that will be particularly tasty. He has narrowed down his ideas to a single recipe he would like to try. He has taken a look at the ingredients he thinks he will need. Sometimes he likes to print out the recipe, but usually he just keeps it open on his ipad. He has made a grocery list and picked up most of the ingredients, but forgot to get one of the spices needed. He scrolls thorugh the comments to see if any reviewers have replaced it, or done without. He cooks it up. His family loves it, and so does he! The leftovers tase great, and he is quite thrilled, so he leaves a comment about how it works well even with some modifications, and a 5-star review for the author.</p>
+				<p>Main Goal</p>
+				<p>Who</p>
+				<p>What</p>
+				<p>Where</p>
+				<p>What Technology</p>
+				<p>James is a little bored with his usual go-to recipes. He is cooking a big meal Saturday for his family, and would like to try something new. He has browsed through a list of currently popular recipes, and he has searched for pork shoulder in ingredients because he thinks that will be particularly tasty. He has narrowed down his ideas to a single recipe he would like to try. He has taken a look at the ingredients he thinks he will need. Sometimes he likes to print out the recipe, but usually he just keeps it open on his ipad. He has made a grocery list and picked up most of the ingredients, but forgot to get one of the spices needed. He scrolls thorugh the comments to see if any reviewers have replaced it, or done without. He cooks it up. His family loves it, and so does he! The leftovers tase great, and he is quite thrilled, so he leaves a comment about how it works well even with some modifications, and a 5-star review for the author.</p>
 			</section>
 			<section>
 				<h1>Interaction Flow</h1>
@@ -49,15 +42,10 @@
 				<h1>Entities</h1>
 			<ul>
 				<li>Photos</li>
-				<li>Recipe: Ingredients list, steps</li>
+				<li>Recipe: Ingredients list, directions</li>
 				<li>Reviews/Comments</li>
 			</ul>
 			</section>
-
-
 		</main>
-
-
-
 	</body>
 </html>
