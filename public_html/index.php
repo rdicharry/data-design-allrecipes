e3d0209615f44d58d3a986529abb6f051a9a99e3
Rebecca Dicharry <rdicharry@gmail.com>
2016-10-12 13:49:29 -0600

    initial commit including beginning of persona section.

---
+++ public_html/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8"/>
+		<!-- add a CSS file -->
+		<!--<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />-->
+		<title>AllRecipes Data Design</title>
+	</head>
+	<body>
+		<!-- Header information -->
+		<header>
+			<h1>Data Design Project - Allrecipes</h1>
+		</header>
+
+		<!-- Content goes here -->
+		<main>
+			<!-- begin section 1 -->
+			<section>
+				<h1>Persona</h1>
+				<p> The user is someone who is researching a recipe. They may be looking for something to make for dinner, or planning a special cooking project (a challenging new recipe, something inspired by a recent meal, planning a special meal for a special occasion). They would like to see the recipe neatly laid out and easy to read. they would like the steps to be carefully written for cooking success. They would like to see reviews of the recipe so they can feel more confident that their time spent cooking will be successful.
+
+				</p>
+				<p>Alternatively, another type of user would be a person who is creating a recipe and would like to share it. This user wants it to be easy to enter ingredients and steps. This user would also like to see comments in order to recieve positive reviews and reinforce their creative culinary endeavors. (Or, they may be interested in criticism that they can use constructively in order to improve their cooking outcomes.)
+				</p>
+
+				<p>The persona is someone who likes cooking and enjoys good food. They would like to see photos and recipe suggestions. They would like to be able to search recipes and share them via email/social media.
+				</p>
+				<!--<img src="img/molecule.png" alt="Caffeine and cAMP">-->
+			</section>
+			<section>Use Case</section>
+			<section>Interaction Flow</section>
+			<section>Entities</section>
+
+
+		</main>
+
+
+
+	</body>
+</html>
