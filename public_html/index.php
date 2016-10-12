ea33c54f10c373fbb45709690205d7c2b677d9c9
Rebecca Dicharry <rdicharry@gmail.com>
2016-10-12 14:28:56 -0600

    Added some entities

--- public_html/index.php
+++ public_html/index.php
@@ -26,20 +26,33 @@
 				<p>The persona is someone who likes cooking and enjoys good food. They would like to see photos and recipe suggestions. They would like to be able to search recipes and share them via email/social media.
 				</p>
 
-				<p>
-					<b>Name:</b> James O'Brien
-					<b>Age:</b> 42
-					<b>Attitudes and Behaviors:</b> James likes good food, enjoys eating, and enjoys cooking. He likes researching the cooking experience, looking for new recipes to try. He enjoys sharing food with and cooking for others. He may spend a substantial amount of time looking for a great recipe to impress his friends and family, or just for himself.
-					<b>Technology:</b> James ejoys spending time online, but he does not have the most up-to-date software/computer system.
-					<b>Frustrations and Needs:</b> James likes recipes to be easy to read and to search. He wants to be able to read reviews in order to get an idea of how successful a recipe will be (in addition to author's comments describing the outcome).
-					<b>Goals:</b>James likes to share recipes he has developed or modified with others. He likes recieving 5-star reviews on creations that are aparticularly successful. He wants to be able to easily share recipes he has created with friends, family, and co-workers.
-				</p>
+
+					<ul>
+					<li><b>Name:</b> James O'Brien</li>
+					<li><b>Age:</b> 42</li>
+					<li><b>Attitudes and Behaviors:</b> James likes good food, enjoys eating, and enjoys cooking. He likes researching the cooking experience, looking for new recipes to try. He enjoys sharing food with and cooking for others. He may spend a substantial amount of time looking for a great recipe to impress his friends and family, or just for himself.</li>
+					<li><b>Technology:</b> James ejoys spending time online, but he does not have the most up-to-date software/computer system.</li>
+					<li><b>Frustrations and Needs:</b> James likes recipes to be easy to read and to search. He wants to be able to read reviews in order to get an idea of how successful a recipe will be (in addition to author's comments describing the outcome).</li>
+					<li><b>Goals:</b>James likes to share recipes he has developed or modified with others. He likes recieving 5-star reviews on creations that are aparticularly successful. He wants to be able to easily share recipes he has created with friends, family, and co-workers.</li>
+					</ul>
+
 				<!--<img src="img/molecule.png" alt="Caffeine and cAMP">-->
 			</section>
-			<section>Use Case</section>
+			<section>
+				<h1>Use Case</h1>
 			<p>James is a little bored with his usual go-to recipes. He is cooking a big meal Saturday for his family, and would like to try something new. He has browsed through a list of currently popular recipes, and he has searched for pork shoulder in ingredients because he thinks that will be particularly tasty. He has narrowed down his ideas to a single recipe he would like to try. He has taken a look at the ingredients he thinks he will need. Sometimes he likes to print out the recipe, but usually he just keeps it open on his ipad. He has made a grocery list and picked up most of the ingredients, but forgot to get one of the spices needed. He scrolls thorugh the comments to see if any reviewers have replaced it, or done without. He cooks it up. His family loves it, and so does he! The leftovers tase great, and he is quite thrilled, so he leaves a comment about how it works well even with some modifications, and a 5-star review for the author.</p>
-			<section>Interaction Flow</section>
-			<section>Entities</section>
+			</section>
+			<section>
+				<h1>Interaction Flow</h1>
+			</section>
+			<section>
+				<h1>Entities</h1>
+			<ul>
+				<li>Photos</li>
+				<li>Recipe: Ingredients list, steps</li>
+				<li>Reviews/Comments</li>
+			</ul>
+			</section>
 
 
 		</main>
